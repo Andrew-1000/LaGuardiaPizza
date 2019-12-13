e42de7e786c1ee4e35df412d3c97e0c3ffa97a75
Andrew-1000 <andruambia95@gmail.com>
2019-12-13 20:20:33 +0300

    Update the script.js file to add new functions to pick what has been input on the index.html file

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function pickFlavour () {
+  var flavor = document.getElementById("flavour").value;
+  return parseInt(flavor);
+}
+
+function pickSize (){
+  var size = document.getElementById("size").value;
+  return parseInt(size);
+}
+
+function pickCrust(){
+  var crust = document.getElementById("crust").value;
+  return parseInt(crust);
+}
+function pickToppings(){
+  var toppings = document.getElementById("toppings").value;
+  return parseInt(toppings);
+}
+function pickQuantity() {
+  var quantity = document.getElementById("quantity").value;
+  return parseInt(quantity);
+}
