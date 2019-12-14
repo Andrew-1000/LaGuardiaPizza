ee4c511f04d45c8b673a0446a532475fbf0d2970
Andrew-1000 <andruambia95@gmail.com>
2019-12-14 09:17:53 +0300

    Update the script.js file with the onclick function

--- js/script.js
+++ js/script.js
@@ -20,3 +20,18 @@ function pickQuantity() {
   var quantity = document.getElementById("quantity").value;
   return parseInt(quantity);
 }
+
+function makeOrder(){
+  var order = (pickFlavour() + pickSize() + pickCrust() + pickToppings()) * pickQuantity();
+  alert ("You have requested for " + pickQuantity("") + " pizza," + "" + " for Kshs. " + (order) + "" + " Thank you!") 
+  // {
+  //   if (confirm('Would you need a delivery?')) {
+  //     prompt("Enter Your Location")
+  // } else {
+  //     close();
+  // }
+
+  // };
+  prompt("Enter Your Location")
+  alert("Your order will be delivered shortly. Delivery fee is Kshs.100 paid upon receipt") 
+}
