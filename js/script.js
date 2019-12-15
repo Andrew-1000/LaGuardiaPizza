908193b071011377d0970b1c27b41cecbf16b451
Andrew-1000 <andruambia95@gmail.com>
2019-12-15 10:22:32 +0300

    Add function in script.js file to perform the arithmetics of selected options

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+var Pizza = (flavour, size, crust, quantity, meats, veggies, cheese); {
+  this.flavour = flavour;
+  this.size = size;
+  this.crust = crust;
+  // this.toppings = toppings;
+  this.quantity = quantity;
+  this.meats = meats;
+  this.veggies = veggies;
+  this.cheese = cheese;
+}
+
 function pickFlavour () {
   var flavor = document.getElementById("flavour").value;
   return parseInt(flavor);
@@ -12,17 +23,29 @@ function pickCrust(){
   var crust = document.getElementById("crust").value;
   return parseInt(crust);
 }
-function pickToppings(){
-  var toppings = document.getElementById("toppings").value;
-  return parseInt(toppings);
-}
+// function pickToppings(){
+//   var toppings = document.getElementById("toppings").value;
+//   return parseInt(toppings);
+// }
 function pickQuantity() {
   var quantity = document.getElementById("quantity").value;
   return parseInt(quantity);
 }
+function pickMeats() {
+  var meats = document.getElementById("meats").value;
+  return parseInt(meats);
+}
+function pickVeggies() {
+  var veggies = document.getElementById("veggies").value;
+  return parseInt(veggies);
+}
+function pickCheese() {
+  var cheese = document.getElementById("cheese").value;
+  return parseInt(cheese);
+}
 
 function makeOrder(){
-  var order = (pickFlavour() + pickSize() + pickCrust() + pickToppings()) * pickQuantity();
+  var order = (pickFlavour() + pickSize() + pickCrust() + pickMeats() + pickVeggies() + pickCheese()) * pickQuantity();
   if (confirm ("You have requested for " + pickQuantity("") + " pizza," + "" + " for Kshs. " + (order) + "" + " Would you like a delivery?")) {
     prompt("Enter Your Location")
   } else {
